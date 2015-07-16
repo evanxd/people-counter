41e3013132a8560d9a6e6314fe60f2e71e2794ce
Evan Xd <itoyxd@gmail.com>
2015-07-16 14:29:18 +0800

    Fix bugs: it will also show people number even no hashchange event is triggered

--- data/content-script.js
+++ data/content-script.js
@@ -1,17 +1,23 @@
 var HOW_MANY_PEOPLE_MESSAGE = '<h2>人數：57/100</h2>';
+var timerId;
 
+showPeopleCounter();
 window.addEventListener('hashchange', function() {
   showPeopleCounter();
 });
 
 function showPeopleCounter() {
-  var timerId = setInterval(function() {
+  if (timerId) {
+    clearInterval(timerId);
+  }
+  timerId = setInterval(function() {
     var element = document.querySelector('._OKe ol');
     if (element) {
       var counter = document.createElement('li');
       counter.innerHTML = HOW_MANY_PEOPLE_MESSAGE;
       element.appendChild(counter);
       clearInterval(timerId);
+      timerId = null;
     }
   }, 500);
 }
