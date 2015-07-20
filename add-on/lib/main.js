bde20d7f9a5441f2b18184f275e80872c5eaee8f
Evan Xd <itoyxd@gmail.com>
2015-07-20 13:39:34 +0800

    Request people number data without cache

--- add-on/lib/main.js
+++ add-on/lib/main.js
@@ -1,14 +1,16 @@
-var self = require("sdk/self");
-var pageMod = require("sdk/page-mod");
-var Request = require("sdk/request").Request;
-var prefs = require("sdk/simple-prefs").prefs;
+var self = require('sdk/self');
+var pageMod = require('sdk/page-mod');
+var prefs = require('sdk/simple-prefs').prefs;
+var Request = require('sdk/request').Request;
+var now = Date.now();
+var url = `${prefs.serverAddress}/?data=peopleNumber&${now}`;
 
 pageMod.PageMod({
   include: '*.google.com.tw',
   contentScriptFile: './content-script.js',
   onAttach: function(worker) {
     Request({
-      url: prefs.serverAddress + '/?data=peopleNumber',
+      url: url,
       onComplete: function (response) {
         var parsed = JSON.parse(response.text);
         worker.port.emit('peopleNumber', parsed);
