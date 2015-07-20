6f2a8b39fea7717c60e9bca7b76e4b4edec6f166
Evan Xd <itoyxd@gmail.com>
2015-07-20 13:13:17 +0800

    Add serverAddress prefs

--- add-on/lib/main.js
+++ add-on/lib/main.js
@@ -1,13 +1,14 @@
 var self = require("sdk/self");
 var pageMod = require("sdk/page-mod");
 var Request = require("sdk/request").Request;
+var prefs = require("sdk/simple-prefs").prefs;
 
 pageMod.PageMod({
   include: '*.google.com.tw',
   contentScriptFile: './content-script.js',
   onAttach: function(worker) {
     Request({
-      url: 'http://localhost:8080/?data=peopleNumber',
+      url: prefs.serverAddress + '/?data=peopleNumber',
       onComplete: function (response) {
         var parsed = JSON.parse(response.text);
         worker.port.emit('peopleNumber', parsed);
